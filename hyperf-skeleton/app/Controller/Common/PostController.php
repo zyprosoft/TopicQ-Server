529203ffa47929a309401dc3872cd648b00f6942
zyprosoft <1003081775@qq.com>
2021-04-09 11:13:24 +0800

    update

---
+++ hyperf-skeleton/app/Controller/Common/PostController.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace App\Controller\Common;
+use ZYProSoft\Controller\AbstractController;
+use ZYProSoft\Http\AuthedRequest;
+use App\Service\PostService;
+use  Hyperf\HttpServer\Annotation\AutoController;
+use Hyperf\Di\Annotation\Inject;
+
+/**
+ * @AutoController (prefix="/common/post")
+ * Class PostController
+ * @package App\Controller\Common
+ */
+class PostController extends AbstractController
+{
+    /**
+     * @Inject
+     * @var PostService
+     */
+    private PostService $service;
+
+    public function create(AuthedRequest $request)
+    {
+        $this->validate([
+            'title' => 'string|required|min:1|max:32|sensitive',
+            'content' => 'string|required|min:10|max:5000|sensitive',
+            'imageList' => 'array|min:1|max:4',
+            'link' => 'string|min:1|max:500|sensitive',
+            'vote' => 'array|min:1',
+            'vote.subject' => 'string|required_with:vote|min:1|max:32|sensitive',
+            'vote.items' => 'array|required_with:vote|min:1|max:10',
+            'vote.items.*.content' => 'string|required_with:vote|min:1|max:32|sensitive'
+        ]);
+        $params = $request->getParams();
+        $result = $this->service->create($params);
+        return $this->success($result);
+    }
+}
--- hyperf-skeleton/app/Service/PostService.php
+++ hyperf-skeleton/app/Service/PostService.php
@@ -4,7 +4,59 @@
 namespace App\Service;
 
 
+use App\Constants\ErrorCode;
+use App\Model\Post;
+use App\Model\Vote;
+use App\Model\VoteItem;
+use Hyperf\DbConnection\Db;
+use ZYProSoft\Exception\HyperfCommonException;
+
 class PostService extends BaseService
 {
+    public function create(array $params)
+    {
+        $post = null;
+        Db::transaction(function () use ($params,&$post) {
+            $title = $params['title'];
+            $content = $params['content'];
+            $link = data_get($params,'link');
+            $imageList = data_get($params,'imageList');
+            $vote = data_get($params,'vote');
 
+            $post = new Post();
+            $post->title = $title;
+            if (mb_strlen($content) < 32) {
+                $post->summary = $content;
+            }
+            $post->content = $content;
+            if (isset($imageList)) {
+                $post->image_list = json_encode($imageList);
+            }
+            if (isset($link)) {
+                $post->link = $link;
+            }
+
+            if (isset($vote)) {
+                $subject = $vote['subject'];
+                $items = collect($vote['items']);
+                $vote = new Vote();
+                $vote->title = $subject;
+                $vote->saveOrFail();
+                $items->map(function (array $item) use ($vote){
+                    $voteItem = new VoteItem();
+                    $voteItem->content = $item['content'];
+                    $voteItem->vote_id = $vote->vote_id;
+                    $voteItem->saveOrFail();
+                });
+                $post->vote_id = $vote->vote_id;
+                $post->saveOrFail();
+            }
+        });
+
+        if (!isset($post)) {
+            throw new HyperfCommonException(ErrorCode::SERVER_ERROR,'发布帖子失败');
+        }
+
+        return $this->success($post);
+    }
 }
