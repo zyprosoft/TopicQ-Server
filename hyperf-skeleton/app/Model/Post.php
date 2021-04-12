3846b04a1de49b3b20d38b3acf7814036db68810
zyprosoft <1003081775@qq.com>
2021-04-12 08:57:06 +0800

    update

--- hyperf-skeleton/app/Model/Post.php
+++ hyperf-skeleton/app/Model/Post.php
@@ -5,26 +5,29 @@ namespace App\Model;
 
 /**
  * @property int $post_id 
- * @property string $title 
- * @property string $summary 
- * @property string $content 
- * @property string $image_list 
- * @property int $owner_id 
- * @property string $link 
- * @property int $vote_id 
- * @property int $read_count 
- * @property int $favorite_count 
- * @property int $forward_count 
- * @property int $comment_count 
- * @property int $audit_status 
- * @property string $audit_note 
- * @property int $is_hot 
- * @property string $last_comment_time 
- * @property int $sort_index 
- * @property int $is_recommend 
+ * @property string $title 标题
+ * @property string $summary 概要
+ * @property string $content 内容
+ * @property string $image_list 图片列表
+ * @property int $owner_id 作者
+ * @property string $link 超链接
+ * @property int $vote_id 投票信息
+ * @property int $read_count 阅读数
+ * @property int $favorite_count 收藏数
+ * @property int $forward_count 转发数
+ * @property int $comment_count 评论数
+ * @property int $audit_status 0审核中1:审核通过-1:审核不通过
+ * @property string $audit_note 审核备注
+ * @property int $is_hot 是否热门帖子0否1是
+ * @property string $last_comment_time 最新一条评论的时间
+ * @property int $sort_index 排序置顶用
+ * @property int $is_recommend 是否推荐帖
  * @property string $deleted_at 
  * @property \Carbon\Carbon $created_at 
  * @property \Carbon\Carbon $updated_at 
+ * @property int $join_user_count 参与用户数
+ * @property-read \App\Model\User $author 
+ * @property-read \App\Model\Vote $vote 
  */
 class Post extends Model
 {
@@ -34,9 +37,7 @@ class Post extends Model
      * @var string
      */
     protected $table = 'post';
-
     protected $primaryKey = 'post_id';
-
     /**
      * The attributes that are mass assignable.
      *
@@ -48,16 +49,13 @@ class Post extends Model
      *
      * @var array
      */
-    protected $casts = ['post_id' => 'integer', 'owner_id' => 'integer', 'vote_id' => 'integer', 'read_count' => 'integer', 'favorite_count' => 'integer', 'forward_count' => 'integer', 'comment_count' => 'integer', 'audit_status' => 'integer', 'is_hot' => 'integer', 'sort_index' => 'integer', 'is_recommend' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
-
-
+    protected $casts = ['post_id' => 'integer', 'owner_id' => 'integer', 'vote_id' => 'integer', 'read_count' => 'integer', 'favorite_count' => 'integer', 'forward_count' => 'integer', 'comment_count' => 'integer', 'audit_status' => 'integer', 'is_hot' => 'integer', 'sort_index' => 'integer', 'is_recommend' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime', 'join_user_count' => 'integer'];
     public function author()
     {
-        return $this->hasOne(User::class,'user_id','owner_id');
+        return $this->hasOne(User::class, 'user_id', 'owner_id');
     }
-
     public function vote()
     {
-        return $this->hasOne(Vote::class,'vote_id','vote_id');
+        return $this->hasOne(Vote::class, 'vote_id', 'vote_id');
     }
 }
