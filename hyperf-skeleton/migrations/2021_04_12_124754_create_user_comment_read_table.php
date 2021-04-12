fcc2d0a4631453dc03c2493e3c39c426474d9d0c
zyprosoft <1003081775@qq.com>
2021-04-12 12:48:04 +0800

    update

---
+++ hyperf-skeleton/migrations/2021_04_12_124754_create_user_comment_read_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Hyperf\Database\Schema\Schema;
+use Hyperf\Database\Schema\Blueprint;
+use Hyperf\Database\Migrations\Migration;
+
+class CreateUserCommentReadTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_comment_read', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_comment_read');
+    }
+}
