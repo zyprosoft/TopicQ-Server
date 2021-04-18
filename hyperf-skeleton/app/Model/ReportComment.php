b3ad254c5c8322f0ec036b4f37d94696c1477c18
zyprosoft <1003081775@qq.com>
2021-04-18 20:17:07 +0800

    update

--- hyperf-skeleton/app/Model/ReportComment.php
+++ hyperf-skeleton/app/Model/ReportComment.php
@@ -11,6 +11,8 @@ namespace App\Model;
  * @property string $deleted_at 
  * @property \Carbon\Carbon $created_at 
  * @property \Carbon\Carbon $updated_at 
+ * @property int $audit_status 0审核中1:审核通过-1:审核不通过
+ * @property string $audit_note 审核备注
  */
 class ReportComment extends Model
 {
@@ -31,5 +33,5 @@ class ReportComment extends Model
      *
      * @var array
      */
-    protected $casts = ['id' => 'integer', 'comment_id' => 'integer', 'owner_id' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+    protected $casts = ['id' => 'integer', 'comment_id' => 'integer', 'owner_id' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime', 'audit_status' => 'integer'];
 }
