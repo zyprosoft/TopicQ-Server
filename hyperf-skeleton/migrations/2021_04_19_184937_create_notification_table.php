814e70abd2cd739c8a350733677f62014dfa8491
zyprosoft <1003081775@qq.com>
2021-04-19 18:55:12 +0800

    update

--- hyperf-skeleton/migrations/2021_04_19_184937_create_notification_table.php
+++ hyperf-skeleton/migrations/2021_04_19_184937_create_notification_table.php
@@ -12,8 +12,21 @@ class CreateNotificationTable extends Migration
     public function up(): void
     {
         Schema::create('notification', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('message_id');
+            $table->bigInteger('user_id')->comment('用户ID');
+            $table->string('title',30)->comment('通知标题');
+            $table->text('content')->comment('通知内容');
+            $table->tinyInteger('is_top')->default(0)->comment('是否置顶0:否1:是');
+            $table->tinyInteger('is_read')->default(0)->comment('是否已读0:否1:是');
+            $table->tinyInteger('level')->default(0)->comment('级别0:普通1:提醒2:错误:3:严重4:拉黑');
+            $table->string('level_label',2)->after('level')->nullable()->comment('自定义通知标签名');
+
+            $table->index('user_id');
             $table->timestamps();
+            $table->softDeletes();
+            $table->engine = "InnoDB";
+            $table->charset = "utf8mb4";
+            $table->collation = "utf8mb4_unicode_ci";
         });
     }
 
