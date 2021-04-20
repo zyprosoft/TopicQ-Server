bd59ddcd25b52170e0074933870d273aad979c75
zyprosoft <1003081775@qq.com>
2021-04-20 18:06:20 +0800

    update

--- hyperf-skeleton/app/Service/UserService.php
+++ hyperf-skeleton/app/Service/UserService.php
@@ -215,8 +215,10 @@ class UserService extends BaseService
             $this->addAuditImage($imageList,$userUpdate->update_id, Constants::IMAGE_AUDIT_OWNER_USER);
         }
 
-        //加入用户资料异步审核任务
-        $this->push(new UserUpdateMachineAuditJob($userUpdate->update_id));
+        //加入用户资料异步审核任务,是否需要审核取决于更新的内容是不是包含图片和昵称
+        if(isset($userUpdate->nickname) || isset($userUpdate->avatar) || isset($userUpdate->background)) {
+            $this->push(new UserUpdateMachineAuditJob($userUpdate->update_id));
+        }
 
         return $this->success();
     }
