435916ba607e9140e29a735c8ebe2063201b3376
zyprosoft <1003081775@qq.com>
2021-04-20 18:20:49 +0800

    update

--- hyperf-skeleton/app/Service/UserService.php
+++ hyperf-skeleton/app/Service/UserService.php
@@ -198,14 +198,15 @@ class UserService extends BaseService
             //检查图片是否审核通过
             if(!empty($imageList)) {
                 $needImageAudit = $this->auditImageOrFail($imageList);
-            }else{
-                Log::info("图片无需再审核，直接更新到用户信息上");
-                //图片都是审核通过的，那么直接更新到对应用户信息就行
-                if (isset($userInfo['avatar'])) {
-                    $user->avatar = $userInfo['avatar'];
-                }
-                if (isset($userInfo['background'])) {
-                    $user->background = $userInfo['background'];
+                if(!$needImageAudit) {
+                    Log::info("图片无需再审核，直接更新到用户信息上");
+                    //图片都是审核通过的，那么直接更新到对应用户信息就行
+                    if (isset($userInfo['avatar'])) {
+                        $user->avatar = $userInfo['avatar'];
+                    }
+                    if (isset($userInfo['background'])) {
+                        $user->background = $userInfo['background'];
+                    }
                 }
             }
 
