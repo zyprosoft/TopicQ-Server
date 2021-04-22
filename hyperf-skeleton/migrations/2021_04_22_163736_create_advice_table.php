a3bd86f85633004cc055c31f0b3ed42a02cdce34
zyprosoft <1003081775@qq.com>
2021-04-22 16:42:53 +0800

    update

--- hyperf-skeleton/migrations/2021_04_22_163736_create_advice_table.php
+++ hyperf-skeleton/migrations/2021_04_22_163736_create_advice_table.php
@@ -13,7 +13,14 @@ class CreateAdviceTable extends Migration
     {
         Schema::create('advice', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('owner_id')->comment('建议者');
+            $table->text('content')->comment('建议内容');
+
+            $table->softDeletes();
             $table->timestamps();
+            $table->engine = "InnoDB";
+            $table->charset = "utf8mb4";
+            $table->collation = "utf8mb4_unicode_ci";
         });
     }
 
