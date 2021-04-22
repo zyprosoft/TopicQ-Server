b9bde4d86f15d08cf4eb86644a88155d3d0f4b84
zyprosoft <1003081775@qq.com>
2021-04-22 18:40:00 +0800

    update

--- hyperf-skeleton/app/Job/CommentHotStatusCheckJob.php
+++ hyperf-skeleton/app/Job/CommentHotStatusCheckJob.php
@@ -2,6 +2,7 @@
 
 
 namespace App\Job;
+use App\Constants\Constants;
 use App\Model\Comment;
 use Hyperf\AsyncQueue\Job;
 use ZYProSoft\Facade\Cache;
@@ -39,14 +40,18 @@ class CommentHotStatusCheckJob extends Job
         // 4.回复数达到10条以上可单独成立 5. 点赞数达到10个以上可单独成立
         $topStarList = Comment::query()->where('post_id',$this->postId)
                                        ->where('praise_count','>=',2)
+                                       ->with(['post'])
                                        ->orderByDesc('praise_count')
                                        ->limit($this->baseTopCount)
                                        ->get();
         $topReplyList = Comment::query()->where('post_id',$this->postId)
                                         ->where('reply_count','>=',2)
+                                        ->with(['post'])
                                         ->orderByDesc('reply_count')
                                         ->limit($this->baseTopCount)
                                         ->get();
+        $topStartKeyList = $topStarList->keyBy('comment_id');
+        $topReplyKeyList = $topReplyList->keyBy('comment_id');
         if ($topStarList->isEmpty() && $topReplyList->isEmpty()) {
             return;
         }
@@ -63,6 +68,7 @@ class CommentHotStatusCheckJob extends Job
             if ($hotCommentIds->isEmpty()) {
                 $hotCommentIds->union($topStarList->slice(0,$chooseCount)->pluck('comment_id'));
             }
+            $hotCommentIds = $hotCommentIds->unique();
         }
         if (! $topReplyList->isEmpty()) {
             //有没有超过5个回复的
@@ -76,6 +82,7 @@ class CommentHotStatusCheckJob extends Job
             if ($hotCommentIds->isEmpty()) {
                 $hotCommentIds->union($topReplyList->slice(0,$chooseCount)->pluck('comment_id'));
             }
+            $hotCommentIds = $hotCommentIds->unique();
         }
         //设置热评
         if (!$hotCommentIds->isEmpty()) {
@@ -83,6 +90,21 @@ class CommentHotStatusCheckJob extends Job
                 ->update(['is_hot'=>1]);
             $hotCommentIdsLabel = $hotCommentIds->toJson();
             Log::info("已经将($hotCommentIdsLabel)设置成热门评论");
+            $hotCommentIds->map(function (int $commentId) use ($topReplyKeyList, $topStartKeyList) {
+                //成为热评给用户发一条通知
+                $title = "评论已成为热门评论";
+                $label = "通知";
+                $level = Constants::MESSAGE_LEVEL_WARN;
+                if(isset($topStarList[$commentId])) {
+                    //
+                    $comment = $topReplyKeyList[$commentId];
+                    $content = "您在帖子《{$comment->post->title}》下发表的评论《{$comment->content}》受到众人的喜爱，已经成为热门评论，请继续努力，感谢您为社区活跃做出的贡献~";
+                    $notification = new AddNotificationJob($comment->owner_id,$title,$content,false,$level);
+                    $notification->levelLabel = $label;
+                    $notification->keyInfo = json_encode(['post_id'=>$comment->post_id]);
+                    $this->push($notification);
+                }
+            });
             return;
         }
         Log::info("帖子($this->postId)暂无热门评论");
