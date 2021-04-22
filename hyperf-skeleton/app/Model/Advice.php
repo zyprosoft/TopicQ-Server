b05f65453c7938a84f40be66e1ab1a21bd8fc3af
zyprosoft <1003081775@qq.com>
2021-04-22 16:51:22 +0800

    update

---
+++ hyperf-skeleton/app/Model/Advice.php
@@ -0,0 +1,34 @@
+<?php
+
+declare (strict_types=1);
+namespace App\Model;
+
+/**
+ * @property int $id 
+ * @property int $owner_id 建议者
+ * @property string $content 建议内容
+ * @property string $deleted_at 
+ * @property \Carbon\Carbon $created_at 
+ * @property \Carbon\Carbon $updated_at 
+ */
+class Advice extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'advice';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [];
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = ['id' => 'int', 'owner_id' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+}
