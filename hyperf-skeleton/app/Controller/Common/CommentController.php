aba498d2bf0e5e4cbb4c1fba4aa30ea2fc183af3
zyprosoft <1003081775@qq.com>
2021-04-23 12:15:51 +0800

    update

--- hyperf-skeleton/app/Controller/Common/CommentController.php
+++ hyperf-skeleton/app/Controller/Common/CommentController.php
@@ -50,8 +50,8 @@ class CommentController extends AbstractController
     public function reply(AuthedRequest $request)
     {
         $this->validate([
-            'content' => 'string|required|min:1|max:500|sensitive',
-            'commentId' => 'integer|required|min:1|exists:comment,comment_id',
+            'content' => 'string|required_without:imageList|min:1|max:500|sensitive',
+            'commentId' => 'integer|required_without:content|min:1|exists:comment,comment_id',
             'imageList' => 'array|min:1|max:4',
             'link' => 'string|min:1|max:500'
         ]);
