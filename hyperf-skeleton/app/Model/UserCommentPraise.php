ba5e6a7f813e67d8f46319bfc8276d1ee3c86135
zyprosoft <1003081775@qq.com>
2021-04-24 05:47:50 +0800

    update

--- hyperf-skeleton/app/Model/UserCommentPraise.php
+++ hyperf-skeleton/app/Model/UserCommentPraise.php
@@ -10,6 +10,9 @@ namespace App\Model;
  * @property \Carbon\Carbon $created_at 
  * @property \Carbon\Carbon $updated_at 
  * @property int $comment_owner_id 评论归属作者
+ * @property int $owner_read_status 评论归属者阅读状态
+ * @property-read \App\Model\User $author 
+ * @property-read \App\Model\Comment $comment 
  */
 class UserCommentPraise extends Model
 {
@@ -30,15 +33,13 @@ class UserCommentPraise extends Model
      *
      * @var array
      */
-    protected $casts = ['id' => 'integer', 'user_id' => 'integer', 'comment_id' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime', 'comment_owner_id' => 'integer'];
-
+    protected $casts = ['id' => 'integer', 'user_id' => 'integer', 'comment_id' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime', 'comment_owner_id' => 'integer', 'owner_read_status' => 'integer'];
     public function author()
     {
-        return $this->hasOne(User::class,'user_id','user_id');
+        return $this->hasOne(User::class, 'user_id', 'user_id');
     }
-
     public function comment()
     {
-        return $this->hasOne(Comment::class,'comment_id','comment_id');
+        return $this->hasOne(Comment::class, 'comment_id', 'comment_id');
     }
 }
