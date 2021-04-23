3c9d277c2a2d6d5304683556e7630fa1fcfbab51
zyprosoft <1003081775@qq.com>
2021-04-23 13:01:18 +0800

    update

--- hyperf-skeleton/migrations/2021_04_23_125344_create_post_draft_table.php
+++ hyperf-skeleton/migrations/2021_04_23_125344_create_post_draft_table.php
@@ -12,8 +12,20 @@ class CreatePostDraftTable extends Migration
     public function up(): void
     {
         Schema::create('post_draft', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('draft_id');
+            $table->string('title',48)->comment('标题');
+            $table->string('summary',32)->comment('概要');
+            $table->text('content')->comment('内容');
+            $table->text('image_list')->nullable()->comment('图片列表');
+            $table->bigInteger('owner_id')->comment('作者');
+            $table->string('link',500)->nullable()->comment('超链接');
+            $table->text('vote')->nullable()->comment('投票信息json数据');
+
+            $table->index('owner_id');
             $table->timestamps();
+            $table->engine = "InnoDB";
+            $table->charset = "utf8mb4";
+            $table->collation = "utf8mb4_unicode_ci";
         });
     }
 
