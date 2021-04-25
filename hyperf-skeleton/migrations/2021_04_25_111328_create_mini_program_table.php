cb74324c2a26caf4bdf8cd8584fe2fd6710b1940
zyprosoft <1003081775@qq.com>
2021-04-25 11:31:21 +0800

    update

--- hyperf-skeleton/migrations/2021_04_25_111328_create_mini_program_table.php
+++ hyperf-skeleton/migrations/2021_04_25_111328_create_mini_program_table.php
@@ -13,7 +13,25 @@ class CreateMiniProgramTable extends Migration
     {
         Schema::create('mini_program', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('app_id',48)->comment('小程序appId');
+            $table->string('icon',500)->comment('图标');
+            $table->string('name',64)->comment('名字');
+            $table->integer('category_id')->comment('分类ID');
+            $table->string('address',500)->comment('主体的地址');
+            $table->string('phone_number',64)->comment('主体的座机');
+            $table->string('mobile',64)->comment('主体的手机信息');
+            $table->string('owner_nickname',64)->comment('主体联系人名字');
+            $table->string('open_time',64)->comment('主体的经营时间');
+            $table->bigInteger('create_user_id')->nullable()->comment('创建者ID');
+            $table->bigInteger('update_user_id')->nullable()->comment('更新者ID');
+
+            $table->unique('app_id');
+            $table->index('category_id');
+            $table->softDeletes();
             $table->timestamps();
+            $table->engine = "InnoDB";
+            $table->charset = "utf8mb4";
+            $table->collation = "utf8mb4_unicode_ci";
         });
     }
 
--- hyperf-skeleton/migrations/2021_04_25_111828_create_mini_program_category_table.php
+++ hyperf-skeleton/migrations/2021_04_25_111828_create_mini_program_category_table.php
@@ -12,8 +12,18 @@ class CreateMiniProgramCategoryTable extends Migration
     public function up(): void
     {
         Schema::create('mini_program_category', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->integerIncrements('category_id');
+            $table->string('name',24)->comment('分类名称');
+            $table->string('icon',500)->comment('分类图标');
+            $table->integer('total')->default(0)->comment('当前分类下总数');
+            $table->bigInteger('create_user_id')->nullable()->comment('创建者');
+            $table->bigInteger('update_user_id')->nullable()->comment('更新者');
+
+            $table->softDeletes();
             $table->timestamps();
+            $table->engine = "InnoDB";
+            $table->charset = "utf8mb4";
+            $table->collation = "utf8mb4_unicode_ci";
         });
     }
 
