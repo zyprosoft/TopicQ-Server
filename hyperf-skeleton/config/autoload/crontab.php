1e3c41320ce548200c5a203beace2290a3009d50
zyprosoft <1003081775@qq.com>
2021-05-02 11:09:43 +0800

    update

--- hyperf-skeleton/config/autoload/crontab.php
+++ hyperf-skeleton/config/autoload/crontab.php
@@ -2,6 +2,7 @@
 use Hyperf\Crontab\Crontab;
 use ZYProSoft\Task\ClearExpireCaptchaTask;
 use ZYProSoft\Task\ClearLogFileTask;
+use App\Task\PostRecommendCalculateTask;
 
 return [
     // 是否开启定时任务
@@ -15,5 +16,9 @@ return [
             ->setRule('5 0 * * *')
             ->setCallback([ClearLogFileTask::class, 'execute'])
             ->setMemo('定时清除过期的验证码'),
+        (new Crontab())->setName('calculateRecommendWeight')
+            ->setRule('*/10 * * * *')
+            ->setCallback([PostRecommendCalculateTask::class, 'execute'])
+            ->setMemo('定时统计帖子推荐权重'),
     ]
 ];
