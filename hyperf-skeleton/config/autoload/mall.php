24457987526b1894c27dded4723984ffef664b26
zyprosoft <1003081775@qq.com>
2021-05-04 16:05:08 +0800

    update

--- hyperf-skeleton/config/autoload/mall.php
+++ hyperf-skeleton/config/autoload/mall.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 return [
     'pdd' => [
-        'pid' => '16116857_206554128',
+        'pid' => '16116857_207075147',
         'name' => '统一推广位',
         'client_id' => env('PDD_CLIENT_ID'),
         'client_secret' => env('PDD_CLIENT_SECRET'),
