16ff28d017f61c9745982a2856825d227a8fafcd
zyprosoft <1003081775@qq.com>
2021-05-06 14:55:19 +0800

    update

---
+++ hyperf-skeleton/app/Service/GoodsService.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Service;
+
+
+class GoodsService extends BaseService
+{
+
+}
--- hyperf-skeleton/migrations/2021_05_06_133152_create_goods_table.php
+++ hyperf-skeleton/migrations/2021_05_06_133152_create_goods_table.php
@@ -24,6 +24,7 @@ class CreateGoodsTable extends Migration
             $table->tinyInteger('status')->default(0)->comment('商品状态');
             $table->bigInteger('total_sale_count')->default(0)->comment('总售出数量');
             $table->string('labels',500)->nullable()->comment('标签');
+            $table->string('desc',1000)->nullable()->comment('商品简介');
 
             $table->index('owner_id');
             $table->index('category_id');
