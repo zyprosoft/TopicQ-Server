bf71131e7adb0c13f606f6624180c0e61bb6892b
zyprosoft <1003081775@qq.com>
2021-05-07 13:31:57 +0800

    update

--- hyperf-skeleton/migrations/2021_05_07_132517_create_subscribe_forum_password_table.php
+++ hyperf-skeleton/migrations/2021_05_07_132517_create_subscribe_forum_password_table.php
@@ -13,7 +13,20 @@ class CreateSubscribeForumPasswordTable extends Migration
     {
         Schema::create('subscribe_forum_password', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('unlock_sn_no',64)->comment('解锁的密钥串');
+            $table->bigInteger('forum_id')->comment('绑定的板块');
+            $table->tinyInteger('status')->comment('0未使用1已使用');
+            $table->bigInteger('owner_id')->comment('被谁领取了');
+
+            $table->unique('unlock_sn_no');
+            $table->index('forum_id');
+            $table->index('status');
+            $table->index('owner_id');
             $table->timestamps();
+            $table->softDeletes();
+            $table->engine = "InnoDB";
+            $table->charset = "utf8mb4";
+            $table->collation = "utf8mb4_unicode_ci";
         });
     }
 
