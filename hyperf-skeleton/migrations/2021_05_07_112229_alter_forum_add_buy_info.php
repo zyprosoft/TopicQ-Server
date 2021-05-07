8ff0ebb0d4ebe29b569f2ae280d0a8e1dabc6a6b
zyprosoft <1003081775@qq.com>
2021-05-07 11:36:15 +0800

    udpate

--- hyperf-skeleton/migrations/2021_05_07_112229_alter_forum_add_buy_info.php
+++ hyperf-skeleton/migrations/2021_05_07_112229_alter_forum_add_buy_info.php
@@ -11,8 +11,15 @@ class AlterForumAddBuyInfo extends Migration
      */
     public function up(): void
     {
-        Schema::table('', function (Blueprint $table) {
+        Schema::table('forum', function (Blueprint $table) {
             //
+            $table->string('password',500)->nullable()->comment('密码');
+            $table->integer('max_member_count')->default(0)->comment('最多订阅用户0为不限制');
+            $table->tinyInteger('need_buy')->default(0)->comment('是否需要付费0否1是');
+            $table->tinyInteger('need_auth')->default(0)->comment('是否需要密码授权');
+            $table->bigInteger('price')->default(0)->comment('付费价格单位分');
+            $table->string('buy_tip',500)->comment('付费提示内容');
+
         });
     }
 
@@ -21,8 +28,14 @@ class AlterForumAddBuyInfo extends Migration
      */
     public function down(): void
     {
-        Schema::table('', function (Blueprint $table) {
+        Schema::table('forum', function (Blueprint $table) {
             //
+            $table->removeColumn('password');
+            $table->removeColumn('max_member_count');
+            $table->removeColumn('need_buy');
+            $table->removeColumn('need_auth');
+            $table->removeColumn('price');
+            $table->removeColumn('buy_tip');
         });
     }
 }
