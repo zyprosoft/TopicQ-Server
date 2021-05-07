8c5c52c11053c51055ffd154966838d14c7426f0
zyprosoft <1003081775@qq.com>
2021-05-08 07:59:48 +0800

    update

--- hyperf-skeleton/app/Service/ForumService.php
+++ hyperf-skeleton/app/Service/ForumService.php
@@ -179,7 +179,7 @@ class ForumService extends BaseService
             $forum->is_subscribe = 1;
             return $forum;
         });
-        return $list->sortByDesc('goods_id')->sortByDesc('need_auth');
+        return $list;
     }
 
     protected function generateUnlockSn(string $prefix)
