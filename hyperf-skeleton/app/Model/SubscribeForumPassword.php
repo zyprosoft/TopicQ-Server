ac5a2c13c67812b54da998a31d1597c4af187013
zyprosoft <1003081775@qq.com>
2021-05-07 14:01:02 +0800

    update

--- hyperf-skeleton/app/Model/SubscribeForumPassword.php
+++ hyperf-skeleton/app/Model/SubscribeForumPassword.php
@@ -4,12 +4,14 @@ declare (strict_types=1);
 namespace App\Model;
 
 /**
- * @property int $id 
- * @property string $unlock_sn_no 解锁的密钥串
+ * @property int $policy_id 
+ * @property string $introduce 本次授权的介绍
+ * @property string $sn_prefix 本次发布的授权码前缀
  * @property int $forum_id 绑定的板块
- * @property int $status 0未使用1已使用
- * @property int $owner_id 被谁领取了
+ * @property int $status 0待领取1已领完-1已作废
  * @property int $price 钥匙串价值单位分0不重要>0需要显示
+ * @property int $total_count 总共可以生成多少张授权
+ * @property int $left_count 剩余多少张可授权
  * @property \Carbon\Carbon $created_at 
  * @property \Carbon\Carbon $updated_at 
  * @property string $deleted_at 
@@ -33,5 +35,5 @@ class SubscribeForumPassword extends Model
      *
      * @var array
      */
-    protected $casts = ['id' => 'int', 'forum_id' => 'integer', 'status' => 'integer', 'owner_id' => 'integer', 'price' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+    protected $casts = ['id' => 'int', 'forum_id' => 'integer', 'status' => 'integer', 'owner_id' => 'integer', 'price' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime', 'policy_id' => 'integer', 'total_count' => 'integer', 'left_count' => 'integer'];
 }
---
+++ hyperf-skeleton/app/Model/UserSubscribePassword.php
@@ -0,0 +1,36 @@
+<?php
+
+declare (strict_types=1);
+namespace App\Model;
+
+/**
+ * @property int $id 
+ * @property string $unlock_sn 最长64位的解锁码
+ * @property int $owner_id 归属谁
+ * @property int $status 0待使用1已使用
+ * @property int $policy_id 归属哪个批次
+ * @property \Carbon\Carbon $created_at 
+ * @property \Carbon\Carbon $updated_at 
+ * @property string $deleted_at 
+ */
+class UserSubscribePassword extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'user_subscribe_password';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [];
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = ['id' => 'int', 'owner_id' => 'integer', 'status' => 'integer', 'policy_id' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+}
