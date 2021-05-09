c1b3a3746b5ba237a95616671d1577379a8ea6f8
zyprosoft <1003081775@qq.com>
2021-05-09 16:31:14 +0800

    update

--- hyperf-skeleton/app/Model/VoucherUseHistory.php
+++ hyperf-skeleton/app/Model/VoucherUseHistory.php
@@ -7,12 +7,11 @@ namespace App\Model;
  * @property int $id 
  * @property int $owner_id 归属者
  * @property string $voucher_sn 券编码
- * @property int $policy_id 批次ID
- * @property int $policy_goods_id 适用商品ID
- * @property int $policy_black_id 不适用商品ID
+ * @property int $voucher_id 券ID
  * @property int $amount 金额
  * @property int $type 0扣减1:回滚
  * @property string $order_no 使用时候的订单号
+ * @property int $order_id 订单编号
  * @property \Carbon\Carbon $created_at 
  * @property \Carbon\Carbon $updated_at 
  * @property string $deleted_at 
@@ -39,7 +38,7 @@ class VoucherUseHistory extends Model
      *
      * @var array
      */
-    protected $casts = ['id' => 'int', 'owner_id' => 'integer', 'policy_id' => 'integer', 'policy_goods_id' => 'integer', 'policy_black_id' => 'integer', 'amount' => 'integer', 'type' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+    protected $casts = ['id' => 'int', 'owner_id' => 'integer', 'policy_id' => 'integer', 'policy_goods_id' => 'integer', 'policy_black_id' => 'integer', 'amount' => 'integer', 'type' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime', 'voucher_id' => 'integer', 'order_id' => 'integer'];
     public function policy()
     {
         return $this->hasOne(VoucherPolicy::class, 'policy_id', 'policy_id');
