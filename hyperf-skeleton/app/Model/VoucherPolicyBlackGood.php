6d32b4991db9b51910a93dc6d32221b679d04cb1
zyprosoft <1003081775@qq.com>
2021-05-09 12:54:03 +0800

    update

--- hyperf-skeleton/app/Model/VoucherPolicyBlackGood.php
+++ hyperf-skeleton/app/Model/VoucherPolicyBlackGood.php
@@ -5,13 +5,12 @@ namespace App\Model;
 
 /**
  * @property int $policy_black_id 
- * @property int $activity_id 活动ID
- * @property int $policy_id 批次ID
  * @property string $category_list 拉黑产品类别，多个ID使用逗号分割,没有goods_list的时候说明是类别下全部拉黑
  * @property string $goods_list 拉黑具体产品，当有指定产品列表的时候，就不存在为类别全部拉黑
  * @property \Carbon\Carbon $created_at 
  * @property \Carbon\Carbon $updated_at 
  * @property string $deleted_at 
+ * @property-read \App\Model\VoucherPolicy $policy 
  */
 class VoucherPolicyBlackGood extends Model
 {
@@ -21,9 +20,7 @@ class VoucherPolicyBlackGood extends Model
      * @var string
      */
     protected $table = 'voucher_policy_black_goods';
-
     protected $primaryKey = 'policy_black_id';
-
     /**
      * The attributes that are mass assignable.
      *
@@ -36,21 +33,18 @@ class VoucherPolicyBlackGood extends Model
      * @var array
      */
     protected $casts = ['policy_black_id' => 'integer', 'activity_id' => 'integer', 'policy_id' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
-
     public function policy()
     {
-        return $this->hasOne(VoucherPolicy::class,'policy_id','policy_id');
+        return $this->hasOne(VoucherPolicy::class, 'policy_id', 'policy_id');
     }
-
     public function category_items()
     {
-        if(empty($this->category_list)) {
+        if (empty($this->category_list)) {
             return [];
         }
         $categoryIds = explode(',', $this->category_list);
         return GoodsCategory::findMany($categoryIds);
     }
-
     public function goods_items()
     {
         if (empty($this->goods_list)) {
--- hyperf-skeleton/app/Model/VoucherPolicyGood.php
+++ hyperf-skeleton/app/Model/VoucherPolicyGood.php
@@ -5,13 +5,12 @@ namespace App\Model;
 
 /**
  * @property int $policy_goods_id 
- * @property int $activity_id 活动ID
- * @property int $policy_id 批次ID
  * @property string $category_list 适用产品类别，多个ID使用逗号分割,没有goods_list的时候说明是类别适用
  * @property string $goods_list 适用具体产品，当有指定产品列表的时候，就不存在为类别通用
  * @property \Carbon\Carbon $created_at 
  * @property \Carbon\Carbon $updated_at 
  * @property string $deleted_at 
+ * @property-read \App\Model\VoucherPolicy $policy 
  */
 class VoucherPolicyGood extends Model
 {
@@ -21,9 +20,7 @@ class VoucherPolicyGood extends Model
      * @var string
      */
     protected $table = 'voucher_policy_goods';
-
     protected $primaryKey = 'policy_goods_id';
-
     /**
      * The attributes that are mass assignable.
      *
@@ -36,21 +33,18 @@ class VoucherPolicyGood extends Model
      * @var array
      */
     protected $casts = ['policy_goods_id' => 'integer', 'activity_id' => 'integer', 'policy_id' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
-
     public function policy()
     {
-        return $this->hasOne(VoucherPolicy::class,'policy_id','policy_id');
+        return $this->hasOne(VoucherPolicy::class, 'policy_id', 'policy_id');
     }
-
     public function category_items()
     {
-        if(empty($this->category_list)) {
+        if (empty($this->category_list)) {
             return [];
         }
         $categoryIds = explode(',', $this->category_list);
         return GoodsCategory::findMany($categoryIds);
     }
-
     public function goods_items()
     {
         if (empty($this->goods_list)) {
