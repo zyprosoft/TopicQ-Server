bcb67d2e0a17316035cdfa1c99bad43dbdf46eee
zyprosoft <1003081775@qq.com>
2021-05-15 14:31:27 +0800

    update

--- hyperf-skeleton/migrations/2021_05_15_142804_create_user_setting_table.php
+++ hyperf-skeleton/migrations/2021_05_15_142804_create_user_setting_table.php
@@ -15,7 +15,8 @@ class CreateUserSettingTable extends Migration
             $table->bigIncrements('id');
             $table->bigInteger('owner_id')->comment('用户ID');
             $table->tinyInteger('open_private_message')->default(1)->comment('默认打开接收私信');
-            
+
+            $table->unique('owner_id');
             $table->timestamps();
             $table->softDeletes();
             $table->engine = "InnoDB";
