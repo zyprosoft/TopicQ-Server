7af44580b0aadcb0dd917c65185575ccc8b20e84
zyprosoft <1003081775@qq.com>
2021-05-15 14:28:10 +0800

    update

--- hyperf-skeleton/app/Model/AppSetting.php
+++ hyperf-skeleton/app/Model/AppSetting.php
@@ -15,6 +15,14 @@ namespace App\Model;
  * @property string $company 公司名
  * @property string $api_version 后端接口版本
  * @property string $theme_color 主题颜色
+ * @property int $custom_theme 是否自定义主题颜色
+ * @property int $theme_gradual 主题色是否渐变
+ * @property int $subscribe_open 是否打开订阅0不打开1打开默认打开
+ * @property int $private_message_open 私信功能是否打开0不打开1打开
+ * @property int $message_on_attention 必须关注才能发消息0不需要1需要
+ * @property int $self_mall_open 自营店铺，默认不打开,不打开的情况下我的订单也被隐藏起来
+ * @property int $red_bag_post 悬赏帖是否打开
+ * @property int $praise_cash_post 赞赏功能是否打开
  * @property \Carbon\Carbon $created_at 
  * @property \Carbon\Carbon $updated_at 
  * @property string $deleted_at 
@@ -38,5 +46,5 @@ class AppSetting extends Model
      *
      * @var array
      */
-    protected $casts = ['id' => 'int', 'custom_no_more' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+    protected $casts = ['id' => 'int', 'custom_no_more' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime', 'custom_theme' => 'integer', 'theme_gradual' => 'integer', 'subscribe_open' => 'integer', 'private_message_open' => 'integer', 'message_on_attention' => 'integer', 'self_mall_open' => 'integer', 'red_bag_post' => 'integer', 'praise_cash_post' => 'integer'];
 }
