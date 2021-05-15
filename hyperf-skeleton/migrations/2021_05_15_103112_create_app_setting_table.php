40b719428b600e2f44674b4d85ca31beaac08248
zyprosoft <1003081775@qq.com>
2021-05-15 10:54:06 +0800

    update

--- hyperf-skeleton/migrations/2021_05_15_103112_create_app_setting_table.php
+++ hyperf-skeleton/migrations/2021_05_15_103112_create_app_setting_table.php
@@ -13,7 +13,22 @@ class CreateAppSettingTable extends Migration
     {
         Schema::create('app_setting', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('app_name',24)->nullable()->comment('名称');
+            $table->text('app_introduce')->nullable()->comment('应用介绍');
+            $table->string('protocol_url',500)->nullable()->comment('用户协议地址');
+            $table->string('about_url',500)->nullable()->comment('关于地址');
+            $table->string('app_version',24)->nullable()->comment('应用版本');
+            $table->string('contact_weixin',64)->nullable()->comment('联系微信');
+            $table->tinyInteger('custom_no_more')->default(0)->comment('自定义没有更多');
+            $table->string('company',64)->nullable()->comment('公司名');
+            $table->string('api_version',24)->nullable()->comment('后端接口版本');
+            $table->string('theme_color')->nullable()->comment('主题颜色');
+
             $table->timestamps();
+            $table->softDeletes();
+            $table->engine = "InnoDB";
+            $table->charset = "utf8mb4";
+            $table->collation = "utf8mb4_unicode_ci";
         });
     }
 
