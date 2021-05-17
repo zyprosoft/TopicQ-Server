8e9edb1c5392083427796bc23ad8e061b0a7c46c
zyprosoft <1003081775@qq.com>
2021-05-17 08:56:49 +0800

    -update

--- hyperf-skeleton/migrations/2021_05_17_084917_create_topic_category_table.php
+++ hyperf-skeleton/migrations/2021_05_17_084917_create_topic_category_table.php
@@ -12,8 +12,15 @@ class CreateTopicCategoryTable extends Migration
     public function up(): void
     {
         Schema::create('topic_category', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('category_id');
+            $table->string('name',12)->comment('话题分类名称');
+
+            $table->unique('name');
             $table->timestamps();
+            $table->softDeletes();
+            $table->engine = "InnoDB";
+            $table->charset = "utf8mb4";
+            $table->collation = "utf8mb4_unicode_ci";
         });
     }
 
--- hyperf-skeleton/migrations/2021_05_17_084924_create_topic_table.php
+++ hyperf-skeleton/migrations/2021_05_17_084924_create_topic_table.php
@@ -12,8 +12,26 @@ class CreateTopicTable extends Migration
     public function up(): void
     {
         Schema::create('topic', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('topic_id');
+            $table->string('title',64)->comment('话题标题');
+            $table->string('introduce',128)->comment('话题介绍');
+            $table->bigInteger('owner_id')->comment('谁创建的');
+            $table->string('image',500)->comment('图片');
+            $table->string('location',64)->comment('位置');
+            $table->bigInteger('category_id')->comment('分类');
+            $table->bigInteger('read_count')->comment('阅读数');
+            $table->bigInteger('join_count')->comment('参与人数');
+            $table->bigInteger('post_count')->comment('帖子数');
+            $table->bigInteger('comment_count')->comment('评论数');
+
+            $table->unique('title');
+            $table->index('category_id');
+            $table->index('owner_id');
             $table->timestamps();
+            $table->softDeletes();
+            $table->engine = "InnoDB";
+            $table->charset = "utf8mb4";
+            $table->collation = "utf8mb4_unicode_ci";
         });
     }
 
