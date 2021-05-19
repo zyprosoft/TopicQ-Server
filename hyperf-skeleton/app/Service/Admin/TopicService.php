3b53574ce1712a8262fa6ebbb75961839f23b345
zyprosoft <1003081775@qq.com>
2021-05-19 19:42:11 +0800

    update

---
+++ hyperf-skeleton/app/Service/Admin/TopicService.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Service\Admin;
+use ZYProSoft\Service\AbstractService;
+
+class TopicService extends AbstractService
+{
+
+}
--- hyperf-skeleton/migrations/2021_05_19_192646_alter_topic_add_sort_index.php
+++ hyperf-skeleton/migrations/2021_05_19_192646_alter_topic_add_sort_index.php
@@ -16,6 +16,7 @@ class AlterTopicAddSortIndex extends Migration
             $table->bigInteger('recommend_weight')->default(0)->comment('推荐权重');
             $table->tinyInteger('sort_index')->default(0)->comment('置顶');
             $table->string('tag',4)->nullable()->comment('自定义标签');
+            $table->tinyInteger('audit_status')->default(0)->comment('0:待审核1通过-1不通过审核状态，话题必须要管理员审核通过');
         });
     }
 
@@ -29,6 +30,7 @@ class AlterTopicAddSortIndex extends Migration
             $table->dropColumn('recommend_weight');
             $table->dropColumn('sort_index');
             $table->dropColumn('tag');
+            $table->dropColumn('audit_status');
         });
     }
 }
