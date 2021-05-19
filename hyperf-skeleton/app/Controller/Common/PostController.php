6493683812880349de3703f75d31e49c0566932e
zyprosoft <1003081775@qq.com>
2021-05-19 10:42:55 +0800

    update

--- hyperf-skeleton/app/Controller/Common/PostController.php
+++ hyperf-skeleton/app/Controller/Common/PostController.php
@@ -302,16 +302,16 @@ class PostController extends AbstractController
         return $this->success($result);
     }
 
-    public function getPostListByTopicId($request)
+    public function getPostListByTopicId()
     {
         $this->validate([
             'pageIndex' => 'integer|required|min:0',
             'pageSize' => 'integer|required|min:10|max:30',
             'topicId' => 'integer|required|exists:topic,topic_id',
         ]);
-        $pageIndex = $request->param('pageIndex');
-        $pageSize = $request->param('pageSize');
-        $topicId = $request->param('topicId');
+        $pageIndex = $this->request->param('pageIndex');
+        $pageSize = $this->request->param('pageSize');
+        $topicId = $this->request->param('topicId');
         $result = $this->service->getPostListByTopicId($pageIndex, $pageSize, $topicId);
         return $this->success($result);
     }
