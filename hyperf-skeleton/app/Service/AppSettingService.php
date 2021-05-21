24a0aea8b1785f6bb367af6f2c9b85708437a8e8
zyprosoft <1003081775@qq.com>
2021-05-21 21:07:48 +0800

    update

--- hyperf-skeleton/app/Service/AppSettingService.php
+++ hyperf-skeleton/app/Service/AppSettingService.php
@@ -11,4 +11,27 @@ class AppSettingService extends AbstractService
     {
         return AppSetting::firstOrFail();
     }
+
+    public function getInfoNoFail()
+    {
+        return AppSetting::first();
+    }
+
+    public static function isUserUploadVideoEnable()
+    {
+        $appSetting = AppSetting::first();
+        if (!$appSetting instanceof AppSetting) {
+            return false;
+        }
+        return  $appSetting->enable_user_video == 1;
+    }
+
+    public static function isUserCreateTopicEnable()
+    {
+        $appSetting = AppSetting::first();
+        if (!$appSetting instanceof AppSetting) {
+            return false;
+        }
+        return  $appSetting->enable_user_create_topic == 1;
+    }
 }
