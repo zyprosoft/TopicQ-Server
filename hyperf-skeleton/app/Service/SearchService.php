2099794709e8c28c80211d9e6eb245795814e56b
zyprosoft <1003081775@qq.com>
2021-05-25 10:07:48 +0800

    update

--- hyperf-skeleton/app/Service/SearchService.php
+++ hyperf-skeleton/app/Service/SearchService.php
@@ -16,15 +16,34 @@ class SearchService extends BaseService
         $post = Post::search($keyword)->get();
         $hasPostMore = 0;
         if ($post->count() > 10) {
-            $post = $post->slice(0,10);
+            $post = $post->slice(0, 10);
             $hasPostMore = 1;
+            $topicIds = $post->pluck('topic_id')->where('topic_id', '>', 0);
+            $topicList = Topic::findMany($topicIds)->keyBy('topic_id');
+            //补充话题
+            $post->map(function (Post $post) use ($topicList) {
+                if (!empty($post->avatar_list)) {
+                    $post->avatar_list = explode(';', $post->avatar_list);
+                } else {
+                    $post->avatar_list = null;
+                }
+                if (!empty($post->image_list)) {
+                    $post->image_list = explode(';', $post->image_list);
+                }
+                if ($post->topic_id > 0) {
+                    $post->topic = $topicList[$post->topic_id];
+                } else {
+                    $post->topic = null;
+                }
+            });
+
         }
 
         //搜索话题
         $topic = Topic::search($keyword)->get();
         $hasTopicMore = 0;
         if ($topic->count() > 10) {
-            $topic = $topic->slice(0,10);
+            $topic = $topic->slice(0, 10);
             $hasTopicMore = 1;
         }
 
@@ -32,17 +51,17 @@ class SearchService extends BaseService
         $user = User::search($keyword)->get();
         $hasUserMore = 0;
         if ($user->count() > 10) {
-            $user = $user->slice(0,10);
+            $user = $user->slice(0, 10);
             $hasUserMore = 1;
         }
 
-        return ['post'=>[
+        return ['post' => [
             'list' => $post,
             'has_more' => $hasPostMore
-        ],'topic'=>[
+        ], 'topic' => [
             'list' => $topic,
             'has_more' => $hasTopicMore
-        ],'user'=>[
+        ], 'user' => [
             'list' => $user,
             'has_more' => $hasUserMore
         ]];
