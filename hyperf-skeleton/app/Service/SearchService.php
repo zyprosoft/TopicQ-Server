564eec29ac100f74534d637cab6eac5761dc1e15
zyprosoft <1003081775@qq.com>
2021-05-25 09:24:01 +0800

    update

--- hyperf-skeleton/app/Service/SearchService.php
+++ hyperf-skeleton/app/Service/SearchService.php
@@ -14,13 +14,37 @@ class SearchService extends BaseService
     {
         //搜索帖子
         $post = Post::search($keyword)->get();
+        $hasPostMore = 0;
+        if ($post->count() > 10) {
+            $post = $post->slice(0,10);
+            $hasPostMore = 1;
+        }
 
         //搜索话题
         $topic = Topic::search($keyword)->get();
+        $hasTopicMore = 0;
+        if ($topic->count() > 10) {
+            $topic = $topic->slice(0,10);
+            $hasTopicMore = 1;
+        }
 
         //搜索用户
         $user = User::search($keyword)->get();
+        $hasUserMore = 0;
+        if ($user->count() > 10) {
+            $user = $user->slice(0,10);
+            $hasUserMore = 1;
+        }
 
-        return ['post'=>$post,'topic'=>$topic,'user'=>$user];
+        return ['post'=>[
+            'list' => $post,
+            'has_more' => $hasPostMore
+        ],'topic'=>[
+            'list' => $topic,
+            'has_more' => $hasTopicMore
+        ],'user'=>[
+            'list' => $user,
+            'has_more' => $hasUserMore
+        ]];
     }
 }
