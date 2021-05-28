0f13b5a149e98054f908f040a875beabed35733c
zyprosoft <1003081775@qq.com>
2021-05-28 17:08:47 +0800

    udpate

--- hyperf-skeleton/app/Model/ScoreAction.php
+++ hyperf-skeleton/app/Model/ScoreAction.php
@@ -5,12 +5,11 @@ namespace App\Model;
 
 /**
  * @property int $id 
- * @property string $name 行为名称
+ * @property string $bind_action 绑定的行为码
+ * @property string $name 行为名称描述
  * @property int $score 积分值默认1
  * @property int $day_once 是否每天限制一次
  * @property int $is_system 是否系统设定的行为
- * @property string $bind_module 绑定触发的大模块，需要是非系统设定的行为才能填充
- * @property string $bind_module_action 绑定触发的具体方法名，需要是非系统设定的行为才能填充
  * @property int $creator 创建者ID,系统初始化的为0
  * @property string $deleted_at 
  * @property \Carbon\Carbon $created_at 
