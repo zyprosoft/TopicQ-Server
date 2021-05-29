95a603a547aaaa4d14d05b946e9d448310e521db
zyprosoft <1003081775@qq.com>
2021-05-29 13:42:32 +0800

    update

--- hyperf-skeleton/migrations/2021_05_29_133851_alter_forum_add_post_user_group.php
+++ hyperf-skeleton/migrations/2021_05_29_133851_alter_forum_add_post_user_group.php
@@ -11,8 +11,9 @@ class AlterForumAddPostUserGroup extends Migration
      */
     public function up(): void
     {
-        Schema::table('', function (Blueprint $table) {
+        Schema::table('forum', function (Blueprint $table) {
             //
+            $table->string('can_post_user_group',500)->nullable()->comment('允许发帖的用户分组,为空的时候，所有用户均可发帖');
         });
     }
 
@@ -21,8 +22,9 @@ class AlterForumAddPostUserGroup extends Migration
      */
     public function down(): void
     {
-        Schema::table('', function (Blueprint $table) {
+        Schema::table('forum', function (Blueprint $table) {
             //
+            $table->dropColumn('can_post_user_group');
         });
     }
 }
