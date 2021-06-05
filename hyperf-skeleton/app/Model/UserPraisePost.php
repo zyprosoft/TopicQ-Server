02c679f1c1d456b286345ce87b8eb519d23ad3bc
zyprosoft <1003081775@qq.com>
2021-06-05 09:54:51 +0800

    update

---
+++ hyperf-skeleton/app/Model/UserPraisePost.php
@@ -0,0 +1,33 @@
+<?php
+
+declare (strict_types=1);
+namespace App\Model;
+
+/**
+ * @property int $id 
+ * @property int $user_id 用户ID
+ * @property int $post_id 帖子ID
+ * @property \Carbon\Carbon $created_at 
+ * @property \Carbon\Carbon $updated_at 
+ */
+class UserPraisePost extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'user_praise_post';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [];
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = ['id' => 'int', 'user_id' => 'integer', 'post_id' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+}
