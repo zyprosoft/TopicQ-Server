ef46c2efa6c0ff2da63606766e429a9fa032b4e6
zyprosoft <1003081775@qq.com>
2021-06-05 14:44:17 +0800

    udpate

--- hyperf-skeleton/app/Model/UserPraisePost.php
+++ hyperf-skeleton/app/Model/UserPraisePost.php
@@ -4,6 +4,13 @@ declare (strict_types=1);
 namespace App\Model;
 
 /**
+ * @property int $id 
+ * @property int $user_id 用户ID
+ * @property int $post_id 帖子ID
+ * @property int $owner_read_status 帖主已读状态
+ * @property int $post_owner_id 帖主
+ * @property \Carbon\Carbon $created_at 
+ * @property \Carbon\Carbon $updated_at 
  * @property-read \App\Model\User $author 
  * @property-read \App\Model\Post $post 
  */
@@ -26,7 +33,7 @@ class UserPraisePost extends Model
      *
      * @var array
      */
-    protected $casts = ['id' => 'int', 'user_id' => 'integer', 'post_id' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+    protected $casts = ['id' => 'int', 'user_id' => 'integer', 'post_id' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime', 'owner_read_status' => 'integer', 'post_owner_id' => 'integer'];
     public function author()
     {
         return $this->hasOne(User::class, 'user_id', 'user_id');
