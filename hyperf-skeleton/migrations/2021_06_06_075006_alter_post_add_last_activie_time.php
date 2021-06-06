6a9c34ac767a16b6367260eaff0bd63a0fe09a98
zyprosoft <1003081775@qq.com>
2021-06-06 18:49:36 +0800

    update

--- hyperf-skeleton/migrations/2021_06_06_075006_alter_post_add_last_activie_time.php
+++ hyperf-skeleton/migrations/2021_06_06_075006_alter_post_add_last_activie_time.php
@@ -11,8 +11,9 @@ class AlterPostAddLastActivieTime extends Migration
      */
     public function up(): void
     {
-        Schema::table('', function (Blueprint $table) {
+        Schema::table('post', function (Blueprint $table) {
             //
+            $table->dateTime('last_active_time')->nullable()->comment('上一次活跃时间，通过更新帖子，或者回复，可以刷新帖子的活跃时间');
         });
     }
 
@@ -21,8 +22,9 @@ class AlterPostAddLastActivieTime extends Migration
      */
     public function down(): void
     {
-        Schema::table('', function (Blueprint $table) {
+        Schema::table('post', function (Blueprint $table) {
             //
+            $table->dropColumn('last_active_time');
         });
     }
 }
