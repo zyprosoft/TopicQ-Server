4f30a43cb534e339b3cf1079bbcdbea6088128da
zyprosoft <1003081775@qq.com>
2021-06-17 20:21:51 +0800

    update

--- hyperf-skeleton/app/Service/Admin/TopicService.php
+++ hyperf-skeleton/app/Service/Admin/TopicService.php
@@ -47,6 +47,7 @@ class TopicService extends BaseService
             throw new HyperfCommonException(ErrorCode::DO_NOT_REPEAT_ACTION);
         }
         $topic->audit_status = $status;
+        $topic->saveOrFail();
         if ($topic->author->role_id < Constants::USER_ROLE_ADMIN) {
             $title = "发布话题审核结果";
             if($status == Constants::STATUS_OK) {
@@ -54,23 +55,23 @@ class TopicService extends BaseService
                 $content = "您发布的话题《{$topic->title}》已通过管理员审核，感谢您对社区文化构建的大力支持~";
                 //用户有没有关注，没有帮助用户自动关注
                 $userAttentionTopic = UserAttentionTopic::query()->where('user_id',$topic->owner_id)
-                                                                 ->where('topic_id',$topic->topic_id)
-                                                                 ->first();
+                    ->where('topic_id',$topic->topic_id)
+                    ->first();
                 if (!$userAttentionTopic instanceof UserAttentionTopic) {
                     $userAttentionTopic = new UserAttentionTopic();
                     $userAttentionTopic->user_id = $topic->owner_id;
                     $userAttentionTopic->topic_id = $topic->topic_id;
                     $userAttentionTopic->saveOrFail();
                 }
+                $levelLabel = "通知";
             }else{
+                $levelLabel = "提醒";
                 $level = Constants::MESSAGE_LEVEL_BLOCK;
                 $content = "很遗憾，您发布的话题《{$topic->title}》未通过管理员审核，仍然感谢您对社区文化构建的积极参与~";
             }
-            $notification = new AddNotificationJob($topic->owner_id,$title,$content,false,$level);
-            $notification->levelLabel = "通知";
+            $notification = new AddNotificationJob($topic->owner_id,$title,$content,false,$level,$levelLabel);
             $this->push($notification);
         }
-        $topic->saveOrFail();
         return $this->success();
     }
 
