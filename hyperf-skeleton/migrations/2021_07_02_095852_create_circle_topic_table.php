64d4c105f20306a3534666629de9ce6cff374f4b
zyprosoft <1003081775@qq.com>
2021-07-02 10:09:59 +0800

    update

--- hyperf-skeleton/migrations/2021_07_02_095852_create_circle_topic_table.php
+++ hyperf-skeleton/migrations/2021_07_02_095852_create_circle_topic_table.php
@@ -12,8 +12,17 @@ class CreateCircleTopicTable extends Migration
     public function up(): void
     {
         Schema::create('circle_topic', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('topic_id');
+            $table->string('title',12)->comment('话题名字');
+            $table->bigInteger('owner_id')->comment('创建者');
+
+            $table->index('owner_id');
+            $table->unique('title');
             $table->timestamps();
+            $table->softDeletes();
+            $table->engine = "InnoDB";
+            $table->charset = "utf8mb4";
+            $table->collation = "utf8mb4_unicode_ci";
         });
     }
 
