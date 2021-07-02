c557b3401ec11963d9613474e8f0243a6a35b608
zyprosoft <1003081775@qq.com>
2021-07-02 11:08:21 +0800

    update

--- hyperf-skeleton/app/Model/CircleTopic.php
+++ hyperf-skeleton/app/Model/CircleTopic.php
@@ -7,6 +7,7 @@ namespace App\Model;
  * @property int $topic_id 
  * @property string $title 话题名字
  * @property int $owner_id 创建者
+ * @property int $circle_id 圈子ID
  * @property \Carbon\Carbon $created_at 
  * @property \Carbon\Carbon $updated_at 
  * @property string $deleted_at 
@@ -19,9 +20,7 @@ class CircleTopic extends Model
      * @var string
      */
     protected $table = 'circle_topic';
-
     protected $primaryKey = 'topic_id';
-
     /**
      * The attributes that are mass assignable.
      *
@@ -33,5 +32,5 @@ class CircleTopic extends Model
      *
      * @var array
      */
-    protected $casts = ['topic_id' => 'integer', 'owner_id' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+    protected $casts = ['topic_id' => 'integer', 'owner_id' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime', 'circle_id' => 'integer'];
 }
