4433d244ef65ae544ef03713c82452dd4d1ed9fb
zyprosoft <1003081775@qq.com>
2021-07-02 10:54:21 +0800

    update

---
+++ hyperf-skeleton/app/Model/CircleTopic.php
@@ -0,0 +1,34 @@
+<?php
+
+declare (strict_types=1);
+namespace App\Model;
+
+/**
+ * @property int $topic_id 
+ * @property string $title 话题名字
+ * @property int $owner_id 创建者
+ * @property \Carbon\Carbon $created_at 
+ * @property \Carbon\Carbon $updated_at 
+ * @property string $deleted_at 
+ */
+class CircleTopic extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'circle_topic';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [];
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = ['topic_id' => 'integer', 'owner_id' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+}
