bc3ad8f4f61f9369175ef1f73cdc6d22703adb49
zyprosoft <1003081775@qq.com>
2021-07-25 16:43:29 +0800

    update

--- hyperf-skeleton/migrations/2021_07_25_163505_create_circle_expert_table.php
+++ hyperf-skeleton/migrations/2021_07_25_163505_create_circle_expert_table.php
@@ -13,7 +13,19 @@ class CreateCircleExpertTable extends Migration
     {
         Schema::create('circle_expert', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('circle_id')->comment('圈子ID');
+            $table->bigInteger('user_id')->comment('用户ID');
+            $table->bigInteger('post_count')->comment('动态数量');
+            $table->bigInteger('comment_count')->comment('评论数量');
+            $table->bigInteger('praise_count')->comment('动态被点赞数量');
+            $table->bigInteger('favorite_count')->comment('动态被收藏数量');
+            $table->bigInteger('recommend')->default(0)->comment('推荐权重');
+
+            $table->unique(['circle_id','user_id']);
             $table->timestamps();
+            $table->engine = "InnoDB";
+            $table->charset = "utf8mb4";
+            $table->collation = "utf8mb4_unicode_ci";
         });
     }
 
