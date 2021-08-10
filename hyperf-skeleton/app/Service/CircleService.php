3fb5f02a5156479fbd2aa7f0dcb6125f522944da
zyprosoft <1003081775@qq.com>
2021-08-10 16:58:19 +0800

    update

--- hyperf-skeleton/app/Service/CircleService.php
+++ hyperf-skeleton/app/Service/CircleService.php
@@ -356,9 +356,8 @@ class CircleService extends BaseService
         $circle = Circle::query()->limit(3)
             ->orderByDesc('recommend_weight')
             ->get();
-        $circleUser = UserCircle::query()->limit(30)
+        $circleUser = UserCircle::query()->selectRaw('distinct user_id,last_active_time')->limit(30)
             ->orderByDesc('last_active_time')
-            ->groupBy(['user_id'])
             ->get()
             ->pluck('author');
         $topic = CircleTopic::query()
