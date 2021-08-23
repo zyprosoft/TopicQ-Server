447bc4a494a811b1ebe0e51139531890866ba87c
zyprosoft <1003081775@qq.com>
2021-08-23 14:32:00 +0800

    update

--- hyperf-skeleton/seeders/create_sign_status_init_data.php
+++ hyperf-skeleton/seeders/create_sign_status_init_data.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Hyperf\Database\Seeders\Seeder;
+use Hyperf\DbConnection\Db;
 
 class CreateSignStatusInitData extends Seeder
 {
@@ -14,5 +15,37 @@ class CreateSignStatusInitData extends Seeder
     public function run()
     {
         //
+        Db::table('sign_status')->insertOrIgnore([
+            [
+                'name' => '单身中',
+            ],
+            [
+                'name' => '暗恋中',
+            ],
+            [
+                'name' => '想恋爱',
+            ],
+            [
+                'name' => '甜恋中',
+            ],
+            [
+                'name' => '养火花',
+            ],
+            [
+                'name' => '考证中'
+            ],
+            [
+                'name' => '找基友'
+            ],
+            [
+                'name' => '考研中',
+            ],
+            [
+                'name' => '备考中',
+            ],
+            [
+                'name' => '找闺蜜'
+            ]
+        ]);
     }
 }
