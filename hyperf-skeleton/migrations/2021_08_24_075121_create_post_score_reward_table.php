aa871e6a97bc110172d785eb6e6eb5b8ef4f93be
zyprosoft <1003081775@qq.com>
2021-08-24 07:57:56 +0800

    update

--- hyperf-skeleton/migrations/2021_08_24_075121_create_post_score_reward_table.php
+++ hyperf-skeleton/migrations/2021_08_24_075121_create_post_score_reward_table.php
@@ -13,7 +13,17 @@ class CreatePostScoreRewardTable extends Migration
     {
         Schema::create('post_score_reward', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('user_id')->comment('打赏人');
+            $table->bigInteger('post_owner_id')->comment('被打赏');
+            $table->bigInteger('post_id')->comment('打赏的帖子或者动态');
+            $table->bigInteger('amount')->comment('数量');
+
+            $table->unique(['user_id','post_id']);//一个人打上帖子只能有一条记录,多次打赏采用叠加
+            $table->index('post_owner_id');
             $table->timestamps();
+            $table->engine = "InnoDB";
+            $table->charset = "utf8mb4";
+            $table->collation = "utf8mb4_unicode_ci";
         });
     }
 
