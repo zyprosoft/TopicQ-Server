089db6ea447eff4e3d95b82532ab51194c8fce23
zyprosoft <1003081775@qq.com>
2021-08-26 15:58:16 +0800

    update

---
+++ hyperf-skeleton/app/Model/Scrapy/Model/Model.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of ZYProSoft/Hyperf-Skeleton.
+ *
+ * @link     http://zyprosoft.lulinggushi.com
+ * @document http://zyprosoft.lulinggushi.com
+ * @contact  1003081775@qq.com
+ * @Company  码动未来信息技术有限公司(ZYProSoft)
+ * @license  GPL
+ */
+declare(strict_types=1);
+namespace App\Model\Scrapy\Model;
+
+use Hyperf\DbConnection\Model\Model as BaseModel;
+use Hyperf\ModelCache\Cacheable;
+use Hyperf\ModelCache\CacheableInterface;
+
+abstract class Model extends BaseModel implements CacheableInterface
+{
+    use Cacheable;
+}
