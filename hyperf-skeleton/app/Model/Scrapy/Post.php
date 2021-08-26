1a3835b6557c104e32ce3d47aec6068810c820aa
zyprosoft <1003081775@qq.com>
2021-08-26 16:02:00 +0800

    update

---
+++ hyperf-skeleton/app/Model/Scrapy/Post.php
@@ -0,0 +1,41 @@
+<?php
+
+declare (strict_types=1);
+namespace App\Model\Scrapy;
+
+/**
+ * @property int $id 
+ * @property int $floor 
+ * @property string $author 
+ * @property string $content 
+ * @property string $time 
+ * @property int $comment_num 
+ * @property int $thread_id 
+ */
+class Post extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'post';
+    /**
+     * The connection name for the model.
+     *
+     * @var string
+     */
+    protected $connection = 'scrapy';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [];
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = ['id' => 'int', 'floor' => 'integer', 'comment_num' => 'integer', 'thread_id' => 'integer'];
+}
---
+++ hyperf-skeleton/app/Model/Scrapy/Thread.php
@@ -0,0 +1,39 @@
+<?php
+
+declare (strict_types=1);
+namespace App\Model\Scrapy;
+
+/**
+ * @property int $id 
+ * @property string $title 
+ * @property string $author 
+ * @property int $reply_num 
+ * @property int $good 
+ */
+class Thread extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'thread';
+    /**
+     * The connection name for the model.
+     *
+     * @var string
+     */
+    protected $connection = 'scrapy';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [];
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = ['id' => 'int', 'reply_num' => 'integer', 'good' => 'integer'];
+}
