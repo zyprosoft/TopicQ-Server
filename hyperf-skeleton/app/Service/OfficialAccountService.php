f921204b9f64927fa14ccf2624f416a825dfe834
zyprosoft <1003081775@qq.com>
2021-09-08 07:57:40 +0800

    update

--- hyperf-skeleton/app/Service/OfficialAccountService.php
+++ hyperf-skeleton/app/Service/OfficialAccountService.php
@@ -83,7 +83,9 @@ class OfficialAccountService extends BaseService
             }
             $user->wx_fa_is_subscribe = $isSubscribe;
             $user->save();
-            return;
+            if(isset($user->wx_fa_subscribe_time)) {
+               return;
+            }
         }
         $user = User::query()->where('wx_fa_open_id',$openId)->first();
         if (!$user instanceof User) {
