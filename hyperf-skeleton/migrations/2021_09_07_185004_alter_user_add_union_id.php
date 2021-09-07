89c474ba489d12ede3896385dff9502bbd6ffb9b
zyprosoft <1003081775@qq.com>
2021-09-07 19:12:35 +0800

    update

--- hyperf-skeleton/migrations/2021_09_07_185004_alter_user_add_union_id.php
+++ hyperf-skeleton/migrations/2021_09_07_185004_alter_user_add_union_id.php
@@ -14,6 +14,11 @@ class AlterUserAddUnionId extends Migration
         Schema::table('user', function (Blueprint $table) {
             //
             $table->string('wx_union_id',128)->nullable()->comment('微信的unionID');
+            $table->string('wx_fa_open_id',128)->nullable()->comment('公众号openID');
+            $table->tinyInteger('wx_fa_is_subscribe')->nullable()->comment('公众号是否关注');
+            $table->timestamp('wx_fa_subscribe_time')->nullable()->comment('订阅时间');
+            $table->string('wx_fa_subscribe_scene')->nullable()->comment('订阅场景');
+            $table->unique('wx_union_id');
         });
     }
 
@@ -25,6 +30,10 @@ class AlterUserAddUnionId extends Migration
         Schema::table('user', function (Blueprint $table) {
             //
             $table->dropColumn('wx_union_id');
+            $table->dropColumn('wx_fa_open_id');
+            $table->dropColumn('wx_fa_is_subscribe');
+            $table->dropColumn('wx_fa_subscribe_time');
+            $table->dropColumn('wx_fa_subscribe_scene');
         });
     }
 }
