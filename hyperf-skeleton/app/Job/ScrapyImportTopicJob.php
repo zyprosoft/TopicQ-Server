30367f794f308c3f712d9ca270690ce2e49e750a
zyprosoft <1003081775@qq.com>
2021-09-09 09:04:40 +0800

    update

--- hyperf-skeleton/app/Job/ScrapyImportTopicJob.php
+++ hyperf-skeleton/app/Job/ScrapyImportTopicJob.php
@@ -82,6 +82,7 @@ class ScrapyImportTopicJob extends Job
         $result = json_decode($result->getBody(),true);
         if($result['code'] == 1 && $result['message'] == 'SUCCESS') {
             $replyList = collect($result['data']['replies']);
+            Log::info("replyList:".json_encode($replyList));
             $posterFloor = $replyList->first;
             Log::info("获取帖子信息:".json_encode($posterFloor));
             //添加帖子
