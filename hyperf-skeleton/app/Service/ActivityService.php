e024bb54b9f8dee8aff67f306e2a10ba3b14e3cd
zyprosoft <1003081775@qq.com>
2021-09-09 16:35:15 +0800

    update

--- hyperf-skeleton/app/Service/ActivityService.php
+++ hyperf-skeleton/app/Service/ActivityService.php
@@ -105,8 +105,8 @@ class ActivityService extends BaseService
         }else{
             $daySignCountCache = $daySignCountCache + rand(0,5);
         }
-        //不能超过总用户数的4/3
-        $daySignCountCache = min($memberCountCache,floor($memberCountCache*0.75));
+        //不能超过总用户数的一半
+        $daySignCountCache = min($memberCountCache,floor($memberCountCache*0.5));
         $this->cache->set('DAY_SIGN_COUNT_KEY',$daySignCountCache);
 
         //检查用户是否关注了公众号
