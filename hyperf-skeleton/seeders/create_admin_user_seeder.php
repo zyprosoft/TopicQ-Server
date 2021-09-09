3c7ab202cfa774ba6d7623142bba35a401020115
zyprosoft <1003081775@qq.com>
2021-09-09 14:17:32 +0800

    update

--- hyperf-skeleton/seeders/create_admin_user_seeder.php
+++ hyperf-skeleton/seeders/create_admin_user_seeder.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Hyperf\Database\Seeders\Seeder;
+use Hyperf\DbConnection\Db;
 
 class CreateAdminUserSeeder extends Seeder
 {
@@ -14,5 +15,11 @@ class CreateAdminUserSeeder extends Seeder
     public function run()
     {
         //
+        Db::table('user')->insertOrIgnore([
+            [
+                'username' => 'admin',
+                'password' => password_hash('admin123',PASSWORD_DEFAULT)
+            ]
+        ]);
     }
 }
