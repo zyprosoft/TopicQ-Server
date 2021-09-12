eadafd90baa9de17c8d66b82212a291585e136e6
zyprosoft <1003081775@qq.com>
2021-09-12 17:44:04 +0800

    update

--- hyperf-skeleton/app/Task/AutoDelayPostTask.php
+++ hyperf-skeleton/app/Task/AutoDelayPostTask.php
@@ -2,6 +2,7 @@
 
 
 namespace App\Task;
+use App\Constants\Constants;
 use App\Service\Scrapy\ImportPostService;
 use App\Service\Scrapy\PostService;
 use Hyperf\Utils\ApplicationContext;
@@ -10,13 +11,16 @@ class AutoDelayPostTask
 {
     public function execute()
     {
-        $mode = env('SCRAPY_MODE');
-        if(isset($mode)) {
-            $importService = ApplicationContext::getContainer()->get(ImportPostService::class);
-            $importService->getOneTopic();
-        }else{{
-            $importService = ApplicationContext::getContainer()->get(PostService::class);
-            $importService->importTopic();
-        }}
+        $enableScrapy = env('SCRAPY_ENABLE');
+        if (isset($enableScrapy) && $enableScrapy == Constants::STATUS_OK) {
+            $mode = env('SCRAPY_MODE');
+            if(isset($mode)) {
+                $importService = ApplicationContext::getContainer()->get(ImportPostService::class);
+                $importService->getOneTopic();
+            }else{{
+                $importService = ApplicationContext::getContainer()->get(PostService::class);
+                $importService->importTopic();
+            }}
+        }
     }
 }
